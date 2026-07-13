b2fbd6dbe45f5eea3469fa35f6665d8c1d5d4768
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-13 13:40:50 +0200

    feat(ft_parse_map): add parsing functions

--- srcs/ft_parse_map.c
+++ srcs/ft_parse_map.c
@@ -6,7 +6,22 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 11:30:56 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/13 11:30:58 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/13 13:40:01 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "bsq.h"
+
+int	ft_parse_header(char *contents, t_map *map, int start)
+{
+	
+}
+
+int	ft_parse_map(char *content, t_map *map)
+{
+	int	start;
+
+	if (!ft_parse_header(content, map, &start))
+		return (0);
+	return (0);
+}
