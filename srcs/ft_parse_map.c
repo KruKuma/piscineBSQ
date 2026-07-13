daaca83d1ba102e30113ad83c975a2d1b2e64c72
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-13 11:31:30 +0200

    feat(ft_parse_map): add function to parse map

--- srcs/ft_parse_map.c
+++ srcs/ft_parse_map.c
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_parse_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/13 09:04:06 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/13 09:58:03 by nfurst           ###   ########.fr       */
+/*   Created: 2026/07/13 11:30:56 by nfurst            #+#    #+#             */
+/*   Updated: 2026/07/13 11:30:58 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "bsq.h"
-
-int	main(int argc, char **argv)
-{
-	char	*file;
-	char	*content;
-	char	*map;
-	
-	if (argc != 2)
-		return (0);
-	file = argv[1];
-	content = ft_read_file(file);
-	if (content == 0)
-	{
-		write(1, "Error\n", 6);
-		return (1);
-	}
-	if (!parse_map(content, &map))
-	{
-		write(2, "map error\n", 10);
-		return (1);
-	}
-	return (0);
-}
