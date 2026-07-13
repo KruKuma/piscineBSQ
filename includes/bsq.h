b0374170e5946a3dad00e94c58b7adda43cd0f5d
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-13 09:14:43 +0200

    feat(includes): add a header file

---
+++ includes/bsq.h
@@ -0,0 +1,26 @@
+#ifndef BSQ_H
+# define BSQ_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+typedef struct s_map
+{
+	int		rows;
+	int		cols;
+	char	empty;
+	char	obstactle;
+	char	full;
+	char	**grid;
+} t_map;
+
+
+typedef struct s_square
+{
+	int	size;
+	int	row;
+	int	col;
+} t_square;
+
+#endif
