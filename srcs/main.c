200f0675488630176c30f60789acc732a0f70bda
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-14 14:22:51 +0200

    chore(main): fix norminette error

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 09:04:06 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/14 13:49:53 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/14 14:22:19 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int argc, char **argv)
 {
-	int i;
+	int	i;
 
 	if (argc < 2)
 		return (ft_print_error(), 0);
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 	{
 		ft_process_file(argv[i]);
 		if (i < argc - 1)
-		write(1, "\n", 1);
+			write(1, "\n", 1);
 		i++;
 	}
 	return (0);
