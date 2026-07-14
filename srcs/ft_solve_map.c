d70acb90eeaf7e7525527b192e0254f41c8d36d1
Arnaud Mathey <arn.mathey@gmail.com>
2026-07-14 13:48:03 +0200

    chore(ft_solve_map): norminette done

--- srcs/ft_solve_map.c
+++ srcs/ft_solve_map.c
@@ -6,7 +6,7 @@
 /*   By: amathey <arn.mathey@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 20:00:22 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/14 13:37:27 by amathey          ###   ########.fr       */
+/*   Updated: 2026/07/14 13:47:20 by amathey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	ft_min(int a, int b, int c)
 	if (c < min)
 		min = c;
 	return (min);
-	}
+}
 
 int	ft_cell_value(t_map *map, int **square_sizes, int row, int col)
 {
@@ -34,12 +34,13 @@ int	ft_cell_value(t_map *map, int **square_sizes, int row, int col)
 		return (0);
 	if (row == 0 || col == 0)
 		return (1);
-	return (ft_min(square_sizes[row - 1][col], 
-		square_sizes[row][col - 1], 
+	return (ft_min(square_sizes[row - 1][col],
+		square_sizes[row][col - 1],
 		square_sizes[row - 1][col - 1]) + 1);
-	}
+}
 
-void	ft_update_largest_square(t_square *largest_square, int size, int row, int col)
+void	ft_update_largest_square(t_square *largest_square, int size,
+	int row, int col)
 {
 	if (size > largest_square->size)
 	{
@@ -47,9 +48,10 @@ void	ft_update_largest_square(t_square *largest_square, int size, int row, int c
 		largest_square->row = row;
 		largest_square->col = col;
 	}
-	}
+}
 
-int	ft_fill_square_sizes(t_map *map, int **square_sizes, t_square *largest_square)
+int	ft_fill_square_sizes(t_map *map, int **square_sizes,
+	t_square *largest_square)
 {
 	int	row;
 	int	col;
@@ -61,7 +63,8 @@ int	ft_fill_square_sizes(t_map *map, int **square_sizes, t_square *largest_squar
 		while (col < map->cols)
 		{
 			square_sizes[row][col] = ft_cell_value(map, square_sizes, row, col);
-			ft_update_largest_square(largest_square, square_sizes[row][col], row, col);
+			ft_update_largest_square(largest_square,
+				square_sizes[row][col], row, col);
 			col++;
 		}
 		row++;
@@ -71,8 +74,8 @@ int	ft_fill_square_sizes(t_map *map, int **square_sizes, t_square *largest_squar
 
 int	ft_solve_map(t_map *map)
 {
-	int	**square_sizes;
-	t_square largest_square;
+	int			**square_sizes;
+	t_square	largest_square;
 
 	square_sizes = ft_create_square_sizes(map);
 	if (square_sizes == 0)
@@ -83,5 +86,5 @@ int	ft_solve_map(t_map *map)
 	ft_fill_square_sizes(map, square_sizes, &largest_square);
 	ft_fill_square(map, largest_square);
 	ft_free_square_sizes(square_sizes, map->rows);
-	return (1); 
-	}
+	return (1);
+}
