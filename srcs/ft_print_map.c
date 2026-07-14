3ef7108f25ec16545e93a9891bce24b4f898d1bb
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-14 13:26:14 +0200

    chore(ft_print_map): fix calls

--- srcs/ft_print_map.c
+++ srcs/ft_print_map.c
@@ -3,27 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amathey <arn.mathey@gmail.com>             +#+  +:+       +#+        */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 10:52:25 by amathey           #+#    #+#             */
-/*   Updated: 2026/07/14 11:56:54 by amathey          ###   ########.fr       */
+/*   Updated: 2026/07/14 13:15:28 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 void	ft_print_map(t_map *map)
 {
-	int	col;
 	int	row;
 
 	row = 0;
 	while (row < map->rows)
 	{
-		write(1, map->rows, map->cols);
+		write(1, map->grid[row], map->cols);
 		write(1, "\n", 1);
 		row++;
 	}
