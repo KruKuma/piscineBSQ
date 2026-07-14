a26d15fdb3168cab6497329dd1b619e2722b5d05
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-14 23:35:49 +0200

    feat(ft_process_error): add a function to process error

---
+++ srcs/ft_process_error.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_process_error.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/14 23:20:53 by nfurst            #+#    #+#             */
+/*   Updated: 2026/07/14 23:20:53 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+int	ft_process_error(char *content, t_map *map)
+{
+	if (content != 0)
+		free(content);
+	ft_free_map(map);
+	ft_print_map_error();
+	return (1);
+}
